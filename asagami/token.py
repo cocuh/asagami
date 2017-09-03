97e960e09b93c7abf1fd9300623c8280e62de8f9
Kosuke Kusano <cocuh.kk@gmail.com>
2017-09-04 00:38:37 +0900

    change to use class in tokensnot named tuple

--- asagami/token.py
+++ asagami/token.py
@@ -1,23 +1,17 @@
 from typing import Dict, List, Union
 
-from collections import namedtuple
-
 TokenAttributes = Dict[str, Union[str, List[str]]]
 
-BlockToken = namedtuple(
-  'BlockToken',
-  field_names=[
-    'name',
-    'body',
-    'attributes',
-  ],
-)
 
-InlineToken = namedtuple(
-  'InlineToken',
-  field_names=[
-    'name',
-    'value',
-    'attributes',
-  ],
-)
+class BlockToken:
+  def __init__(self, name: str, body: str, attributes: TokenAttributes):
+    self.name = name
+    self.body = body
+    self.attributes = attributes
+
+
+class InlineToken:
+  def __init__(self, name: str, value: str, attributes: TokenAttributes):
+    self.name = name
+    self.value = value
+    self.attributes = attributes
